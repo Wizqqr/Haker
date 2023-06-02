e3044cdd52492254543b47308a5466a09b809f2a
Wizqqr <baban1979molotov@gmail.com>
2023-06-02 18:29:34 +0600

    Ozero

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Button/Button.css'
+
+const Button =(props)=>{
+    const {text,onClick}=props
+    return(
+        <div className="button">
+Al Nassr
+        </div>
+    )
+}
+
+export default Button
